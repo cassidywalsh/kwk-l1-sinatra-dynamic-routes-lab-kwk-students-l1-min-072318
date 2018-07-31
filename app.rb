1b7f59170adacad037ccaa1dfa7a8638499a9dac
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-31 20:11:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,11 +29,11 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
   "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
 end
 
-# get '/:operation/:number1/:number2' do
-#   @operation = [:operation]
-#   @number1 = [:number1]
-#   @number2 = [:number2]
-#   if @operation = "+"
+get '/:operation/:number1/:number2' do
+  @operation = [:operation]
+  @number1 = [:number1]
+  @number2 = [:number2]
+  if @operation = "+"
     
     
 end
