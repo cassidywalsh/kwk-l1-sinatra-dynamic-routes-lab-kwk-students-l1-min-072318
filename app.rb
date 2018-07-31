09a1773ba75db40252e48f2f924d38b765383737
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-31 19:37:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,13 +10,18 @@ get '/square/:number' do
   (num1*num1).to_s
 end
 get '/say/:number/:phrase' do
+  @number = params[:number]
+  @phrase = params[:phrase]
   
 get '/say/:word1/:word2/:word3/:word4/:word5' do
   @word1 = params[:word1]
   @word2 = params[:word2]
   @word3 = params[:word3]
-@word4 = params[:word4]
-@word5 = params[:word5]
-"#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+  "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
 end
+get '/:operation/:number1/:number2' do
+  @operation = [:operation]
+  @number1 = []
 end
